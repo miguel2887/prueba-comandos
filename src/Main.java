0c4592a558e0b59c59dad31db216476890521d9a
miguel2887 <miguel.nieto280387@gmail.com>
2025-02-19 15:32:48 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("miguel angel nieto rodriguez");
+        System.out.println("prueba de comando");
+        
     }
 }
